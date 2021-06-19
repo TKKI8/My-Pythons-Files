db9e59f59f2c66b0dd41748d0dc2b71643dbb617
TKKI8 <86154495+TKKI8@users.noreply.github.com>
2021-06-19 19:29:51 +0700

    eh
    ehhhh

---
+++ Problaby My Programs/Free Robux!.py
@@ -0,0 +1,6 @@
+print("How many robuxs do you want?")
+user_name = input()
+
+if user_name == "1M robuxs please":
+    while 3 > 2:
+        print("LOL GOT HACKED")
---
+++ Problaby My Programs/eda.py
@@ -0,0 +1,6 @@
+print("why you black?")
+user_name = input()
+
+if user_name == "pedro":
+    while 3 > 2:
+        print("now pedro is your awnser?!!?")
